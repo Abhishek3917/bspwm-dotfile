855e394a6664277a4e6a34438c992bae6c709008
abhishek <abhishekskumar709@gmail.com>
2025-02-12 13:34:53 +0530

    files added

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #setting up bspwm
 
-sudo pacman -S ttf-jetbrains-mono-nerd ttf-fira-code starship alacritty mpv feh ueberzug ranger thunar polybar --noconfirm --needed
-
+sudo pacman -S ttf-jetbrains-mono-nerd ttf-fira-code starship alacritty mpv feh ueberzug ranger thunar polybar zsh-syntax-highlighting zsh-autosuggestions chromium zsh --noconfirm --needed
+chsh -l
+echo "set default shell"
+read -r SHELL
+chsh -s $SHELL
 mv -v config/alacritty ../.config/
 mv -v config/starship.toml ../.config/
 mkdir ../Downloads
@@ -9,9 +12,12 @@ mkdir ../Desktop
 mkdir ../Pictures
 mkdir ../.config/polybar
 mv -v Pictures ../
-mv -v polybar ../.config/
+mv -v config/polybar ../.config/
+mv -v config/sxhkd/ ../.config/
+chmod +x ../.config/bspwm/bspwmrc
+chmod +x ../.config/sxhkd/sxhkdrc
+chmod +x ../.config/polybar/launch.sh
 ranger --copy-config=all
 rm .config/ranger/rc.conf
-mv config/ranger/rc.conf ../.config/ranger
-feh --bg-scale ../Pictures/tianshu-liu-SBK40fdKbAg-unsplash.jpg
+mv -v config/ranger/rc.conf ../.config/ranger
 sed -i '53i ~/.fehbg &' ~/.xinitrc
