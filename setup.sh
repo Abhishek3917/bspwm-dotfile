c252cadf73e50b2a6f6c4a157bb581cb6f937abe
abhishek <abhishekskumar709@gmail.com>
2025-02-12 11:46:57 +0530

    files added

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #setting up bspwm
 
-sudo pacman -S ttf-jetbrains-mono-nerd ttf-fira-code starship alacritty mpv --noconfirm --needed
+sudo pacman -S ttf-jetbrains-mono-nerd ttf-fira-code starship alacritty mpv feh ueberzug ranger thunar polybar --noconfirm --needed
 
 mv -v config/alacritty ../.config/
-cp -v config/starship.toml ../.config/
+mv -v config/starship.toml ../.config/
 mkdir ../Downloads
 mkdir ../Desktop
 mkdir ../Pictures
+mkdir ../.config/polybar
 mv -v Pictures ../
+mv -v polybar ../.config/
+ranger --copy-config=all
+rm .config/ranger/rc.conf
+mv config/ranger/rc.conf ../.config/ranger
+feh --bg-scale ../Pictures/tianshu-liu-SBK40fdKbAg-unsplash.jpg
+sed -i '53i ~/.fehbg &' ~/.xinitrc
